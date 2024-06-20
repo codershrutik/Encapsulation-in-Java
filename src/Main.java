2d748ee1050ea01926fda9e4a0375793c3bf3320
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-20 04:13:53 +0000

    Setters used for encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[] args) {
         Person person = new Person("John Cena",35,"Male");
+        person.setName("Archana");
+        person.setAge(45);
         System.out.println(person);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,27 @@
 public class Person{
 
-    public String name = "Shruti";
-    public int age = 21;
-    public String gender = "Female";
+    private String name = "Shruti";
+    private int age = 21;
+    private String gender = "Female";
 
     public Person(String name, int age, String gender){
         this.name = name;
         this.age = age;
         this.gender = gender;
+    }
+
+    //Setters can give some access to other classes
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public boolean setAge(int age){
+        if(age<0 || age>100){
+            return false;
+        } else{
+            this.age = age;
+            return true;
+        }
     }
 
     public String toString(){
